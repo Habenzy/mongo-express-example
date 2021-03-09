8784ca3795920195834eddbe8da32fc641c672e6
Bob <rstauss@burlingtoncodeacademy.com>
2021-03-09 13:05:44 -0500

    read functionality converted to methods

--- library.js
+++ library.js
@@ -1,69 +1,60 @@
 const { MongoClient, ObjectId } = require("mongodb");
-const readline = require("readline");
 
-//readline set up to allow interaction w/ terminal
-const interface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+class DataStore {
 
-async function ask(question) {
-  return new Promise((resolve, reject) => {
-    interface.question(question, resolve);
-  });
-}
+  constructor(dbUrl, database, collection) {
+    this.url = dbUrl
+    this.dbName = database
+    this.collName = collection
+    this.isConnected = false
+  }
+  
+  async openConnect() {
+    if(!isConnected) {
+      const client = new MongoClient(this.url, { useUnifiedTopology: true });
+      await client.connect();
+      const dataBase = client.db(this.dbName);
+      const collection = dataBase.collection(this.collName);
+      this.isConnected = client
 
-//set connection object for mongoDB database server
-let dbUrl = "mongodb://localhost:27017";
-const client = new MongoClient(dbUrl, { useUnifiedTopology: true });
-
-async function populateDb() {
-  //Connect to MongoDB
-  await client.connect();
-  //Find specific database named "library"
-  const libraryDB = client.db("library");
-  //look for collection "books" inside DB named "library"
-  const bookStore = libraryDB.collection("books");
-  //use reference to collection to perform read/write methods
-  let num = 1;
-
-  while (num < 100) {
-    await bookStore.insertOne({ title: num, author: "Bob Stauss" });
-    num++;
+      return collection
+    } else {
+      const dataBase = this.isConnected.db(this.dbName);
+      const collection = dataBase.collection(this.collName);
+      return collection
+    }
   }
 
-  //close the connection
-  await client.close();
-}
-
-async function start() {
-  await client.connect();
-  const dataBase = client.db("library");
-  const collection = dataBase.collection("books");
-
-  console.log("Welcome to the library!");
-  let answer = await ask(
-    "What would you like to do? (see all, find book, add book, update book, remove book) "
-  );
-
   //Print all books in collection
-  if (answer === "see all") {
+  async getAll() {
+    const collection = await this.openConnect()
     //find all documents in collection, and assign the cursor
     let cursor = await collection.find({});
 
     //iterate over the cursor to display all books
-    cursor.forEach((book) => {
-      console.log(book);
+    let resultArr = await cursor.map((book) => {
+      return book
     });
-  } else if (answer === "find book") {//Search by specific parameters
 
-    let searchType = await ask("are you searching by author, or title? ");
-    let titleAuthor = await ask("what are you looking for? ");
+    return resultArr
+
+  } 
+
+
+  async searchByKey(searchType, value) {//Search by specific parameters
+    const collection = this.openConnect()
     //square brackets allow variable values to be used as keys
-    let cursor = collection.find({ [searchType]: titleAuthor });
+    let cursor = await collection.find({ [searchType]: value });
     //iterate over cursor to display all matching results
-    cursor.forEach((book) => console.log(book));
-  } else if (answer === "add book") { //create entry for database
+    let resultArr = await cursor.map((book) => {
+      return book
+    });
+
+    return resultArr
+  }
+
+
+  else if (answer === "add book") { //create entry for database
 
     let resOne = await ask("What is the title of the book? ");
     let resTwo = await ask("Who is the author? ");
@@ -71,7 +62,8 @@ async function start() {
     //get user input, and format for DB entry, implicit scheme
     await collection.insertOne({ title: resOne, author: resTwo });
 
-  } else if (answer === "update book") {
+  } 
+  else if (answer === "update book") {
 
     //show the user all available options
     let cursor = await collection.find({});
@@ -93,7 +85,8 @@ async function start() {
       { _id: ObjectId(bookToUpdate) },//look for a doc that matches this criteria
       { $set: { author: newAuthor, title: newTitle } }//update to be applied, marked by atomic operator $set
     );
-  } else if (answer === "remove book") {//delete a document
+  } 
+  else if (answer === "remove book") {//delete a document
     //tell the user the operations that are available
     let cursor = await collection.find({});
     console.log("all available books:\n");
@@ -106,10 +99,6 @@ async function start() {
     //delete the doc that matches the query
     collection.deleteOne({_id: ObjectId(remove)})
   }
-
-  // await client.close();
-  // console.log("Goodbye!");
-  // process.exit();
 }
 
 // start();
